29fb4476ca9edcd1e91d99506c4736204c4dbec7
keerthana1958 <keerthana@qaoncloud.com>
2025-07-10 22:02:04 +0530

    Add files via upload

---
+++ griuping test.spec.js
@@ -0,0 +1,31 @@
+import{test,expect} from '@playwright/test';
+import { describe } from 'node:test';
+
+test.describe.only('block 1',async()=>{
+test('test1',async({page})=>
+
+{
+console.log('keerthana')
+
+})
+test('test2',async({page})=>
+
+{
+console.log('keerthana is')
+
+})
+})
+test.describe('block 2',async()=>{
+test('test3',async({page})=>
+
+{
+console.log('keerthana is a')
+
+})
+test('test4',async({page})=>
+
+{
+console.log('keerthana is a good girl')
+
+})
+})
